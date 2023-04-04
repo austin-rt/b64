8df940ebc16d878511c5007a337866db0655af0f
Austin Ryan Taylor <97855093+austin-rt@users.noreply.github.com>
2023-04-04 09:52:36 -0400

    break

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,33 @@
 import Head from 'next/head';
-import { Inter } from 'next/font/google';
 import styles from '@/styles/Home.module.css';
-
-const inter = Inter({ subsets: ['latin'] });
+import { useState } from 'react';
+import Image from 'next/image';
 
 export default function Home() {
+  const [selectedFiles, setSelectedFiles] = useState<
+    { fileName: string; dataUri: string }[]
+  >([]);
+
+  const fileSelectedHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files) {
+      const files: any[] = [];
+      for (let i = 0; i < event.target.files.length; i++) {
+        const file = event.target.files[i];
+        const reader = new FileReader();
+        reader.onload = () => {
+          if (reader.result) {
+            files.push({
+              fileName: file.name,
+              dataUri: reader.result
+            });
+          }
+        };
+        reader.readAsDataURL(file);
+        setSelectedFiles(files);
+      }
+    }
+  };
+
   return (
     <>
       <Head>
@@ -19,16 +42,36 @@ export default function Home() {
         />
       </Head>
       <main className={styles.main}>
-        <label
-          className='new-button'
-          htmlFor='convert'
-        >
-          Convert Files
-        </label>
-        <input
-          id='convert'
-          type='file'
-        />
+        <div>
+          <label
+            className='button'
+            htmlFor='convert'
+          >
+            Convert Files
+          </label>
+          <input
+            id='convert'
+            type='file'
+            multiple
+            onChange={fileSelectedHandler}
+          />
+        </div>
+        <div className='image-wrapper'>
+          {selectedFiles.length > 0 &&
+            selectedFiles.map(file => (
+              <div
+                key={`${file.fileName}-${Date.now()}`}
+                className=''
+              >
+                <h3>{file.fileName}</h3>
+                <img
+                  src={file.dataUri}
+                  alt={file.fileName}
+                  className='thumbnail'
+                />
+              </div>
+            ))}
+        </div>
       </main>
     </>
   );
