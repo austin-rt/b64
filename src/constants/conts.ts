cba0d6f29d1a8bc1df1f806a8c3f90306573e9f4
Austin Ryan Taylor <97855093+austin-rt@users.noreply.github.com>
2023-04-05 13:10:02 -0400

    correct endpoints

--- src/constants/conts.ts
+++ src/constants/conts.ts
@@ -1,9 +1,10 @@
 export const BASE_URL = {
-  API: 'https://api.b64.app'
+  API: 'http://localhost:3001'
+  // API: 'https://api.b64.app',
 };
 
 export const API_ENDPOINTS = {
   AUTH: {
-    GOOGLE: '/auth/google'
+    GOOGLE: 'auth/google'
   }
 };
