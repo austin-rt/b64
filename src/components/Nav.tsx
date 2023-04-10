e5d05ae901718aab64d2e211ac1812f986792e61
Austin Ryan Taylor <97855093+austin-rt@users.noreply.github.com>
2023-04-10 14:29:28 -0400

    start nav conditional render

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,18 +1,35 @@
 import Link from 'next/link';
+import Image from 'next/image';
 import { BASE_URL, API_ENDPOINTS } from '@/constants/consts';
+import { useAppSelector } from '@/redux/store';
 
 const Nav = () => {
-  return (
+  const user = useAppSelector(state => state.UserSlice.user);
+
+  return !!user.name ? (
+    <nav>
+      <div>hello, {user.name}</div>
+      <Image
+        src={user.avatar}
+        width={100}
+        height={100}
+        alt={`${user.name}'s profile picture`}
+        unoptimized
+      />
+      <ul>
+        <li>
+          <Link href={`${BASE_URL.API}/${API_ENDPOINTS.AUTH.GOOGLE.LOGOUT}`}>
+            Logout
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  ) : (
     <nav>
       <ul>
         <li>
           <Link href={`${BASE_URL.API}/${API_ENDPOINTS.AUTH.GOOGLE.LOGIN}`}>
             Login With Google
-          </Link>
-        </li>
-        <li>
-          <Link href={`${BASE_URL.API}/${API_ENDPOINTS.AUTH.GOOGLE.LOGOUT}`}>
-            Logout
           </Link>
         </li>
       </ul>
