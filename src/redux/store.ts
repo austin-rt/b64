7937943432e4386737d4f43f86fdccbd99ce40b7
Austin Ryan Taylor <97855093+austin-rt@users.noreply.github.com>
2023-04-10 14:26:34 -0400

    finish redux store setup

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -21,4 +21,10 @@ export const store = configureStore({
   reducer: persistedReducer,
   middleware: [thunk]
 });
+
+export const useAppDispatch: () => typeof store.dispatch = useDispatch;
+export const useAppSelector: TypedUseSelectorHook<
+  ReturnType<typeof store.getState>
+> = useSelector;
+
 export default store;
