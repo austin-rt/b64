025c8fb3893e79edfe181d5c3c39e3fc2b1df3f3
Austin Ryan Taylor <97855093+austin-rt@users.noreply.github.com>
2023-04-12 08:14:43 -0400

    scaffold profile page

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,4 +1,34 @@
+import { useAppSelector } from '@/redux/store';
+import Image from 'next/image';
+
 const Profile = () => {
-  return <div>Profile</div>;
+  const user = useAppSelector(state => state.UserSlice.user);
+
+  return (
+    <div>
+      <Image
+        src={user.avatar}
+        width={200}
+        height={200}
+        unoptimized
+        alt={`${user.name} avatar`}
+      />
+      <div>{user.name}</div>
+      <div>{user.email}</div>
+      <div>
+        {user.images.map(image => (
+          <div key={image.fileName}>
+            <Image
+              src={image.dataURI}
+              width={200}
+              height={200}
+              unoptimized
+              alt={`${image.fileName}`}
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 export default Profile;
